b1ebcfb12236c15a1e4498136f23ba7e3c37d26b
frohlichramon01 <frohlichramon01@gmail.com>
2025-01-26 23:52:42 +0000

    Mod 14 - Exercício

--- product/serializers/category_serializer.py
+++ product/serializers/category_serializer.py
@@ -6,4 +6,4 @@ from product.models.category import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fiels = ['title','slug','description','active',]
+        fields = ['title','slug','description','active']
---
+++ product/tests/test_product_serializer.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from product.factories import CategoryFactory, ProductFactory
+from product.serializers import ProductSerializer
+
+
+class TestProductSerializer(TestCase):
+    def setUp(self) -> None:
+        self.category = CategoryFactory(title="Categoria1")
+        self.product_1 = ProductFactory(
+            title="garrafa de agua", price=50, category=[self.category]
+        )
+        self.product_serializer = ProductSerializer(self.product_1)
+
+    def test_product_serializer(self):
+        serializer_data = self.product_serializer.data
+        self.assertEqual(serializer_data["price"], 50)
+        self.assertEqual(serializer_data["title"], "garrafa de agua")
+        self.assertEqual(
+            serializer_data["category"][0]["title"], "Categoria1")
