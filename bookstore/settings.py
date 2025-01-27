3b1a15ce31287509ca3e8b966bc859bd517d5199
frohlichramon01 <frohlichramon01@gmail.com>
2025-01-27 15:57:09 +0000

    exercicio mod 17

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -133,5 +133,10 @@ INTERNAL_IPS = ['127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
